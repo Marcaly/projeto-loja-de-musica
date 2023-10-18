98d3548015f735707da526c6bfbfbd6f0c86f90e
Gabryel Marçal <byelmarcal@hotmail.com>
2023-10-18 14:00:22 -0300

    implementei o metodo UserDetails na classe User

--- loja-de-musica/src/main/java/projeto/lojademusica/domain/entities/User.java
+++ loja-de-musica/src/main/java/projeto/lojademusica/domain/entities/User.java
@@ -1,10 +1,13 @@
 package projeto.lojademusica.domain.entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Table(name = "users")
 @Entity
@@ -12,11 +15,44 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+@EqualsAndHashCode(of = "id")
+public class User implements UserDetails
+{
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
     private String login;
     private String password;
     private UserRoles role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == UserRoles.ADMIN) return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"), new SimpleGrantedAuthority("ROLE_USER"));
+        else return List.of(new SimpleGrantedAuthority("ROLE ADMIN"));
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
