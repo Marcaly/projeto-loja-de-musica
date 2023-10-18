7a3c7e875af674aab043eabe53ea920bf5aaed4c
Gabryel Marçal <byelmarcal@hotmail.com>
2023-10-18 10:34:24 -0300

    configurei mysql no docker-compose.yml

---
+++ loja-de-musica/src/main/java/projeto/lojademusica/LojaDeMusicaApplication.java
@@ -0,0 +1,13 @@
+package projeto.lojademusica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaDeMusicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaDeMusicaApplication.class, args);
+	}
+
+}
